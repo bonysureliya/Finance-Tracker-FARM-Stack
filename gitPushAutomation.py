01c5586b68df474f3363f1a8d5b011cbb64d0085
bonysureliya <bonysueliya20@gmail.com>
2023-04-16 13:57:42 -0700

    comments added

--- gitPushAutomation.py
+++ gitPushAutomation.py
@@ -1,4 +1,4 @@
 import os 
-
+# commit message here
 commitMessage : str  = input("Commit message : ")
 os.system(f'git add . && git commit -m "{commitMessage}" && git push origin main && git pull origin main')
